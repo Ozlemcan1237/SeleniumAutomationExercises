e0d463ec4e0b8076121224c79a20dd0f4a78f51e
Ozlemcan1237 <sutsuzsutlac@gmail.com>
2022-12-31 14:33:12 +0300

    31/12/22

--- src/test/java/automationexercises/Test08Fatih.java
+++ src/test/java/automationexercises/Test08Fatih.java
@@ -13,7 +13,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
-public class Test008Fatih {
+public class Test08Fatih {
 
     WebDriver driver;
 
@@ -36,25 +36,32 @@ public class Test008Fatih {
     public void test1() {
             //2. 'http://automationexercise.com' URL'sine gidin
             driver.get("http://automationexercise.com");
+
             //3. Ana sayfanın başarıyla göründüğünü doğrulayın
             WebElement anasayfaDogrulama = driver.findElement(By.xpath("//*[@lang='en']"));
             Assert.assertTrue(anasayfaDogrulama.isDisplayed());
+
             //4. 'Ürünler' düğmesine tıklayın
             driver.findElement(By.xpath("//*[@href='/products']")).click();
             driver.navigate().refresh();
             driver.navigate().to("https://automationexercise.com/products");
+
             //5. Kullanıcının TÜM ÜRÜNLER sayfasına başarıyla yönlendirildiğini doğrulayın
             WebElement urunlerSyfsiMiDogrula = driver.findElement(By.xpath("//*[text()='All Products']"));
             Assert.assertTrue(urunlerSyfsiMiDogrula.isDisplayed());
+
             //6. Ürün listesi görünür
             WebElement urunListesiGorunurMu = driver.findElement(By.xpath("//*[@class='features_items']"));
             Assert.assertTrue(urunListesiGorunurMu.isDisplayed());
+
             //7. İlk ürünün 'Ürünü Görüntüle'ye tıklayın
             WebElement urunGoruntule = driver.findElement(By.xpath("//*[@href='/product_details/1']"));
             urunGoruntule.sendKeys(Keys.ENTER);
+
             //8. Kullanıcı ürün detay sayfasına yönlendirildi
             WebElement urunDetaySayfasi = driver.findElement(By.xpath("//*[@class='col-sm-7']"));
             Assert.assertTrue(urunDetaySayfasi.isDisplayed());
+
             //9. Ürün adı, kategorisi, fiyatı, bulunabilirliği, durumu, markası gibi ayrıntıların göründüğünü doğrulayın
             WebElement urunAyrintiSayfasi = driver.findElement(By.xpath("//*[@class='product-information']"));
             Assert.assertTrue(urunAyrintiSayfasi.isDisplayed());
--- src/test/java/automationexercises/Test09.java
+++ src/test/java/automationexercises/Test09.java
@@ -40,21 +40,26 @@ public class Test09 {
         WebElement homePage = driver.findElement(By.xpath("//*[@lang='en']"));
         Assert.assertTrue(homePage.isDisplayed());
         // driver.switchTo().alert().dismiss();
+
         //4. Click on 'Products' button
         driver.navigate().to("https://automationexercise.com/products");
         WebElement products = driver.findElement(By.xpath("//*[@href='/products']"));
         products.click();
+
         //5. Verify user is navigated to ALL PRODUCTS page successfully
         WebElement allProducts = driver.findElement(By.xpath("//div[@style='height: auto !important;']"));
         Assert.assertTrue(allProducts.isEnabled());
+
         //6. Enter product name in search input and click search button
         WebElement searchProduct = driver.findElement(By.xpath("//input[@id='search_product']"));
         searchProduct.sendKeys("POLO");
         WebElement button = driver.findElement(By.xpath("//*[@type='button']"));
         button.click();
+
         //7. Verify 'SEARCHED PRODUCTS' is visible
         WebElement searchedProduct = driver.findElement(By.xpath("//div[@class='features_items']"));
         Assert.assertTrue(searchedProduct.isDisplayed());
+
         //8. Verify all the products related to search are visible
         WebElement searchedAllProducts = driver.findElement(By.xpath("//div[@class='features_items']"));
         Assert.assertTrue(searchedAllProducts.isDisplayed());
